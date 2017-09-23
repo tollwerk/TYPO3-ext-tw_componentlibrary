b09419290823f6fa545de9061b61a3b066243aff
Joschi Kuphal <joschi@kuphal.net>
2017-09-23 14:51:34 +0200

    Release 0.2.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,24 +9,22 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'tollwerk TYPO3 Component Library',
-	'description' => 'TYPO3 Component Library',
-	'category' => 'misc',
-	'author' => 'Joschi Kuphal',
-	'author_email' => 'joschi@tollwerk.de',
-	'state' => 'beta',
-	'internal' => '',
-	'uploadfolder' => '0',
-	'createDirs' => '',
-	'clearCacheOnLoad' => 0,
-	'version' => '0.2.3',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '7.6.0-8.99.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
+    'title' => 'tollwerk TYPO3 Component Library',
+    'description' => 'TYPO3 Component Library',
+    'category' => 'misc',
+    'author' => 'Joschi Kuphal',
+    'author_email' => 'joschi@tollwerk.de',
+    'state' => 'beta',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '0.2.4',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '7.6.0-8.99.99',
+        ),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
 );
