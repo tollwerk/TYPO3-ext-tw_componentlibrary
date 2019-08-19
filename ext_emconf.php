cbbb5b10b49c1ecec852f3cdeac7f68c804f7284
Joschi Kuphal <joschi@tollwerk.de>
2019-08-19 06:54:35 +0200

    Bump TYPO3 version

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version'          => '1.0.0',
     'constraints'      => array(
         'depends'   => array(
-            'typo3' => '10.0.0-10.99.99',
+            'typo3' => '9.5.0-10.99.99',
         ),
         'conflicts' => array(),
         'suggests'  => array(),
